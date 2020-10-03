20246aef1ca441f2b9fbf3fbe42f658f01c97d05
Mihail Gerganchev <mi60y@abv.bg>
2020-10-03 20:35:29 +0300

    Added Car POJO

---
+++ Project/src/main/java/com/safety/car/models/entity/Car.java
@@ -0,0 +1,90 @@
+package com.safety.car.models.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car_details")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "cubic_capacity")
+    private int cubicCapacity;
+
+    @Column(name = "registration_date")
+    private String registrationDate;
+
+    @Column(name = "has_accidents")
+    private boolean hasAccidents;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    public Car() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getCubicCapacity() {
+        return cubicCapacity;
+    }
+
+    public void setCubicCapacity(int cubicCapacity) {
+        this.cubicCapacity = cubicCapacity;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public boolean isHasAccidents() {
+        return hasAccidents;
+    }
+
+    public void setHasAccidents(boolean hasAccidents) {
+        this.hasAccidents = hasAccidents;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
