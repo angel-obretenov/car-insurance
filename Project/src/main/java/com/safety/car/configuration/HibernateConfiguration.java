daa92cba1e6099922a67b86749bc9b79d551c8b2
Mihail Gerganchev <mi60y@abv.bg>
2020-10-03 20:14:07 +0300

    Added HibernateConfiguration

---
+++ Project/src/main/java/com/safety/car/configuration/HibernateConfiguration.java
@@ -0,0 +1,55 @@
+package com.safety.car.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@EnableTransactionManagement
+public class HibernateConfiguration {
+
+    private final String dbDriverClassName;
+    private final String dbUrl;
+    private final String dbUsername;
+    private final String dbPassword;
+
+    public HibernateConfiguration(Environment environment) {
+        dbDriverClassName = environment.getProperty("spring.datasource.driverClassName");
+        dbUrl = environment.getProperty("spring.datasource.url");
+        dbUsername = environment.getProperty("spring.datasource.username");
+        dbPassword = environment.getProperty("spring.datasource.password");
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        var sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.safety.car.models.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        var dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(dbDriverClassName);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        var properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        return properties;
+    }
+}
---
+++ Project/src/main/java/com/safety/car/models/entity/PolicyDetails.java
@@ -0,0 +1,4 @@
+package com.safety.car.models.entity;
+
+public class PolicyDetails {
+}
