03eaba15685c3c16932f3baa564d0badb6711e0c
Angel Obretenov <angel_obretenov@yahoo.com>
2020-10-04 12:42:02 +0300

    MulticriteriaTable pojo

---
+++ Project/src/main/java/com/safety/car/models/entity/MulticriteriaTable.java
@@ -0,0 +1,65 @@
+package com.safety.car.models.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "multicriteria_table")
+public class MulticriteriaTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "cc_min")
+    private Integer ccMin;
+
+    @Column(name = "cc_max")
+    private Integer ccMax;
+
+    @Column(name = "car_age_min")
+    private Integer carMinAge;
+
+    @Column(name = "car_age_max")
+    private Integer carMaxAge;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCcMin() {
+        return ccMin;
+    }
+
+    public void setCcMin(Integer ccMin) {
+        this.ccMin = ccMin;
+    }
+
+    public Integer getCcMax() {
+        return ccMax;
+    }
+
+    public void setCcMax(Integer ccMax) {
+        this.ccMax = ccMax;
+    }
+
+    public Integer getCarMinAge() {
+        return carMinAge;
+    }
+
+    public void setCarMinAge(Integer carMinAge) {
+        this.carMinAge = carMinAge;
+    }
+
+    public Integer getCarMaxAge() {
+        return carMaxAge;
+    }
+
+    public void setCarMaxAge(Integer carMaxAge) {
+        this.carMaxAge = carMaxAge;
+    }
+}
